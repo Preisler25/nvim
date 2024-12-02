1a05809078bf0537635c4f60b1040e74d22a2f9f
Preisler25 <preisler.andras@gmail.com>
2024-12-02 14:30:30 +0100

    lsp-fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,6 +35,9 @@ return { -- add pyright to lspconfig
 
       -- setting custom keymap
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "Show hover information" })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = 0, desc = "Rename symbol" })
+      vim.keymap.set("n", "<leader>oa", vim.lsp.buf.code_action, { desc = "Code action" })
+      vim.keymap.set("n", "<leader>od", vim.diagnostic.open_float, { desc = "Opens diagnostic" })
     end,
   },
 }
