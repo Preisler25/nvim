3be8d95d189f23a8b2615eb8422c1f218133e285
Preisler25 <preisler.andras@gmail.com>
2025-01-07 23:45:54 +0100

    formatting is working :D

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,62 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  init = function()
+    -- Install the conform formatter on VeryLazy
+    LazyVim.on_very_lazy(function()
+      LazyVim.format.register({
+        name = "conform.nvim",
+        priority = 100,
+        primary = true,
+        format = function(buf)
+          require("conform").format({ bufnr = buf })
+        end,
+        sources = function(buf)
+          local ret = require("conform").list_formatters(buf)
+          ---@param v conform.FormatterInfo
+          return vim.tbl_map(function(v)
+            return v.name
+          end, ret)
+        end,
+      })
+    end)
+  end,
+  opts = function()
+    ---@type conform.setupOpts
+    local opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false,
+        quiet = false,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        dart = { "dart_format" }, -- Add Dart formatter here
+      },
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+        dart_format = {
+          command = "dart",
+          args = { "format", "$FILENAME" }, -- Pass the current file path to dart format
+          stdin = false, -- Dart format works on files, not stdin
+        },
+      },
+    }
+    return opts
+  end,
+}
