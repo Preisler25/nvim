a79d73a79fa13365abed1a8a7585f54e0dbeeea5
Preisler25 <preisler.andras@gmail.com>
2025-01-16 21:14:33 +0100

    making it transparent

--- lua/plugins/design.lua
+++ lua/plugins/design.lua
@@ -1,25 +1,37 @@
 return {
-  -- add gruvbox and nord
-
+  -- Add gruvbox, nord, and other colorschemes
   { "ellisonleao/gruvbox.nvim" },
   { "shaunsingh/nord.nvim" },
   { "folke/tokyonight.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true, -- Enable transparency
+      integrations = {
+        -- Add plugin integrations for transparency if needed
+        nvimtree = true,
+        telescope = true,
+        -- Add more integrations as necessary
+      },
+    },
+  },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to use catppuccin-mocha
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
     },
   },
-  -- change trouble config
+
+  -- Enable and configure trouble.nvim
   {
     "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
   },
 
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  -- Uncomment the following line to disable trouble.nvim instead:
+  -- { "folke/trouble.nvim", enabled = false },
 }
